2f65554074777c22be87312b7d85e8487ce3ad57
fahad <fahad@awan90@yahoo.com>
2018-02-08 15:18:24 +0500

    npm installed and admin master page added

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,22 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
+
+
+mix.styles([
+	'resources/assets/css/libs/blog-post.css',
+	'resources/assets/css/libs/bootstap.css',
+	'resources/assets/css/libs/font-awesome.css',
+	'resources/assets/css/libs/metisMenu.css',
+	'resources/assets/css/libs/sb-admin-2.css'
+	//'resources/assets/css/libs/',
+], 'public/css/libs.css');
+
+mix.scripts([
+	'resources/assets/js/libs/jquery.js',
+	'resources/assets/js/libs/bootstrp.js',
+	'resources/assets/js/libs/metisMenu.js',
+	'resources/assets/js/libs/sb-admin-2.js',
+	'resources/assets/js/libs/scripts.js'
+	//'resources/assets/js/libs/',
+], 'public/js/libs.js');
